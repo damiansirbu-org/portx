9c5f0a4adc55951abf29f1c70fb06772cee5c30c
dami.sirbu <dami.sirbu@gmail.com>
2025-11-28 15:26:54 +0200

    Use bundled PowerShell Core instead of system powershell.exe

--- portx.cmd
+++ portx.cmd
@@ -1,3 +1,3 @@
 @echo off
 REM PORTX 2.0 Universal Package Manager
-powershell.exe -ExecutionPolicy Bypass -File "%~dp0ps\portx-import.ps1" %*
+"%~dp0packages\powershell-core\pwsh.exe" -ExecutionPolicy Bypass -File "%~dp0ps\portx-import.ps1" %*
